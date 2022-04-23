163d1f9913979a95a1b5b2f92db6c840e776fbab
bcoop <bcoop@live.unc.edu>
2022-04-23 15:57:17 -0400

    I need to start making smaller commits

---
+++ server.js
@@ -0,0 +1,134 @@
+const express = require('express')
+const app = express()
+
+// get command line args
+const args = require('minimist')(process.argv.slice(2));
+
+// could be command line argument...
+const port = args.port || process.env.PORT || 5000
+
+// make the server
+const server = app.listen(port, () => {
+    console.log(`App running on port ${port}`)
+  });
+
+
+//// a04 database stuff ////
+
+const morgan = require('morgan')
+const errorhandler = require('errorhandler')
+const fs = require('fs')
+
+// require database
+const db = require("./database.js")
+
+
+// Store help text
+const help = (`
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+// If --help or -h, echo help text to STDOUT and exit
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
+}
+
+
+
+
+// bring all the functions from coin assignment
+function coinFlip() {
+  if (Math.random() > 0.5) {
+    return "heads";
+  } else {
+    return "tails";
+  }
+}
+
+function coinFlips(flips) {
+  const res = [];
+  for(let step = 0; step < flips; step++) {
+    res[step] = coinFlip();
+  }
+  return res;
+}
+
+function countFlips(array) {
+  let head_count = 0;
+  let tail_count = 0;
+  for(let step = 0; step < array.length; step++) {
+    if (array[step] == "heads") {
+      head_count++;
+    } else if (array[step] == "tails") {
+      tail_count++;
+    }
+  }
+  if (head_count == 0) {
+    return {'tails': tail_count}
+  } else if (tail_count == 0) {
+    return {'heads': head_count}
+  } else {
+    return {'heads': head_count, 'tails': tail_count}
+  }
+}
+
+function flipACoin(call) {
+  let flipped = coinFlip();
+  let result = "lose";
+  if (flipped == call) {
+    result = "win";
+  }
+  return { call: call, flip: flipped, result: result };
+}
+
+// initial app status
+app.get('/app', (req, res) => {
+  res.status(200).end('OK')
+  res.type('text/plain')
+})
+
+// endpoint for flip
+app.get('/app/flip', (req, res) => {
+  res.status(200).json({ 'flip': coinFlip() })
+  res.type('application/json')
+});
+
+// endpoint for flips/:number
+app.get('/app/flips/:number', (req, res) => {
+  const raw = coinFlips(req.params.number)
+  const summary = countFlips(raw)
+  res.status(200).json({ 'raw': raw, 'summary': summary })
+  res.type('application/json')
+});
+
+// endpoint for call/heads
+app.get('/app/flip/call/heads', (req, res) => {
+  res.status(200).json(flipACoin('heads'))
+  res.type('application/json')
+});
+
+// endpoint for call/tails
+app.get('/app/flip/call/tails', (req, res) => {
+  res.status(200).json(flipACoin('tails'))
+  res.type('application/json')
+});
+
+
+// non-existent endpoint handling
+app.use(function (req, res) {
+  res.status(404).end('404 NOT FOUND')
+  res.type("text/plain")
+});
